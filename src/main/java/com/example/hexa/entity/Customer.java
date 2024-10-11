74099142d388c3d9fc4ce97b3bf0fffe16aabe03
igorpardinho <pardinhoigor@gmail.com>
2024-10-11 09:20:08 -0300

    create entity Customer

---
+++ src/main/java/com/example/hexa/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.example.hexa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userDocument;
+
+    private String creditCartToken;
+
+    private Long value;
+}
+
