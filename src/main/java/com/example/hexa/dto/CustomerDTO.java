9ee3782f387935c4cf155d8b093819e3a67f023d
igorpardinho <pardinhoigor@gmail.com>
2024-10-11 09:20:32 -0300

    create class CustomerDTO

---
+++ src/main/java/com/example/hexa/dto/CustomerDTO.java
@@ -0,0 +1,32 @@
+package com.example.hexa.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CustomerDTO {
+
+    private Long id;
+    @NotBlank
+    private String userDocument;
+
+    @NotBlank
+    private String creditCartToken;
+
+    @NotNull
+    private Long value;
+
+
+
+
+
+
+
+}
