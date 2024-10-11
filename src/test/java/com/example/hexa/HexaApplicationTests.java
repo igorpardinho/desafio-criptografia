103f76ea66323b753b21c519d7bd8330f1b7f43a
igorpardinho <pardinhoigor@gmail.com>
2024-10-11 09:19:19 -0300

    initial commit files

---
+++ src/test/java/com/example/hexa/HexaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO nao esta sendo utilizado testes
+    }
+
+}
